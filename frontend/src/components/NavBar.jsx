fb285d7ab13d1a101b2c31660f88ea96f79e9098
Dhiraj Taye <77880018+dhiraj415@users.noreply.github.com>
2024-10-22 21:03:49 +0530

    second commit

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -24,7 +24,7 @@ const NavBar = () => {
   return (
     <div className="flex items-center justify-between py-5 font-medium">
       <Link to="/">
-        <img src={assets.logo} alt="" />
+        <img className="w-44" src={assets.logo} alt="" />
       </Link>
 
       <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
